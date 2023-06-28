f4db5a00ed99f7753e1fd4a13965bdfc0813acb4
IT21440236 <it21440236@my.sliit.lk>
2023-06-28 17:45:55 +0530

    commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intarr = new int[10]; //This is how u created an array
+
+        ArrayList<Integer> arrayList = new ArrayList<>(); //This is how u create an ArrayList
+
+        Integer[] integerArr = new Integer[10];
+
+        Vector<Integer> vector = new Vector<>();
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+    }
+}
